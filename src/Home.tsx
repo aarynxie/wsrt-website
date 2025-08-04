ebe275a220f4dd01d9d4011f6dc8020da672d220
aarynxie <150180442+aarynxie@users.noreply.github.com>
2025-08-04 19:02:30 -0400

    added project section

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,4 +1,5 @@
 import styles from "./Home.module.css";
+import astraText from "./assets/uw astra title.svg";
 
 export default function Home() {
   return (
@@ -17,7 +18,48 @@ export default function Home() {
         </div>
         <div className={styles.overlaysun}></div>
       </div>
-      <div className={styles.projectscontainer}>projects</div>
+      <div className={styles.projectscontainer}>
+        <div className={styles.projectsheader}>
+          <h1>Our Projects</h1>
+        </div>
+        <div className={styles.projectscontent}>
+          <div className={styles.projectscontenttopbox}>
+            <img src={astraText}></img>
+            <p>Adaptive Signal Transmission and Real-time Analytics</p>
+          </div>
+          <div className={styles.projectscontentbottombox}>
+            <div className={styles.projectscontentbottomleft}>
+              <h2>WSST</h2>
+              <p>Waterloo Space Soldering Team</p>
+            </div>
+            <div className={styles.projectscontentbottomright}>
+              <h2>See all projects</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.footer}>
+        <div className={styles.footertop}>
+          <div className={styles.footertext}>
+            <h2>Let's connect</h2>
+            <p>
+              Whether you're a student, sponsor, or just curious, don’t hesitate
+              to reach out!
+            </p>
+          </div>
+          <div className={styles.footerlinks}>
+            <a href="mailto:uwspaceresearchteam@gmail.com">Email</a>
+            <a href="https://www.instagram.com/uwspaceresearch/">Instagram</a>
+            <a href="https://www.linkedin.com/company/uw-space-research/">
+              LinkedIn
+            </a>
+          </div>
+        </div>
+        <div className={styles.footerbottom}>
+          <p>Made with 💜 in Waterloo, ON</p>
+          <small>© Waterloo Space Research Team 2025</small>
+        </div>
+      </div>
     </>
   );
 }
